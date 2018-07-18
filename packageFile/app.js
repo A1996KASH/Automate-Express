13bead12ea05d18f5151fd7062480fbdf94f5fa8
Akash Sengar <akashsengar96@gmail.com>
2018-07-18 15:24:39 +0530

    Moved On

---
+++ packageFile/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require('path');
+//const postRoutes = require("./routes/posts");
+const app = express();
+/* mongoose
+  .connect("mongodb://localhost:27017/meanApp")
+  .then(() => {
+    console.log("connected");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  }); */
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-with,Content-Type,Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+//app.use("/api/posts",postRoutes);
+app.use("",(req, res, next) => {
+  res.send("Hello World");
+});
+module.exports = app;
--- shell/createStructure.sh
+++ shell/createStructure.sh
@@ -1,5 +1,10 @@
 mkdir $1
+mkdir ./$1/routes
+mkdir ./$1/models
 touch ./$1/index.js
 touch ./$1/package.json
+touch ./$1/server.js
 cat ./packageFile/package.json >> ./$1/package.json
+cat ./packageFile/app.js >> ./$1/index.js
+cat ./packageFile/server.js >> ./$1/server.js
 current_dir=$PWD;cd ./$1;npm install ;cd $current_dir;
