2bd71d4206b8e7800287efaa3f855af3872ee28d
Akash Sengar <akashsengar96@gmail.com>
2018-07-18 11:08:31 +0530

    Changes

--- shell/createStructure.sh
+++ shell/createStructure.sh
@@ -1,4 +1,5 @@
-echo "$1"
 mkdir $1
-touch ./$1/package.json
 touch ./$1/index.js
+touch ./$1/package.json
+cat ./packageFile/package.json >> ./$1/package.json
+current_dir=$PWD;cd ./$1;npm install ;cd $current_dir;
